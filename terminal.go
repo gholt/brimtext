9b7056df5fc2327c75fad12e6f9861218c040ec3
Greg Holt <greg@brim.net>
2014-07-04 13:53:15 -0500

    Initial commit

---
+++ terminal.go
@@ -0,0 +1,26 @@
+// Copyright 2014 Gregory Holt. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package brimtext
+
+import (
+	"code.google.com/p/go.crypto/ssh/terminal"
+	"os"
+)
+
+// GetTTYWidth returns the width of controlling TTY if it can or 80.
+func GetTTYWidth() int {
+	var tty *os.File
+	var err error
+	if tty, err = os.OpenFile("/dev/tty", os.O_RDWR, 0600); err != nil {
+		tty = os.Stdout
+	} else {
+		defer tty.Close()
+	}
+	if width, _, err := terminal.GetSize(int(tty.Fd())); err != nil {
+		return 80
+	} else {
+		return width - 1
+	}
+}
